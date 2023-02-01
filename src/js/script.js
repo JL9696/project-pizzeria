464dc437fcbf76eb233e9a07c7f39ecfbad65ec7
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-02-01 11:49:26 +0100

    render inMenu

--- src/js/script.js
+++ src/js/script.js
@@ -52,11 +52,26 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
-
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log('new Product: ', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+      /* generate HTMl based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
@@ -86,7 +101,7 @@
       thisApp.initData();
       thisApp.initMenu();
     },
-  }
+  };
 
   app.init();
 }
