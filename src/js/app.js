579245748ce6092c30448f1835aeeee09f99b6ee
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-02-19 13:22:22 +0100

    add code booking.js, add code to app.js and serrings.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames,} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function() {
@@ -54,6 +55,13 @@ const app = {
     }
   },
 
+  initBooking: function(){
+    const thisApp = this;
+
+    const widgetContainer = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(widgetContainer); 
+  },
 
   initMenu: function(){
     const thisApp = this;
@@ -109,6 +117,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
